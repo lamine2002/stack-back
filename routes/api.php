4410644e856774aa68718c3670542552ffca0dae
Niang Mohamed Lamine <106320478+lamine2002@users.noreply.github.com>
2024-06-22 19:29:17 +0000

    Ajout des du route group middleware

--- routes/api.php
+++ routes/api.php
@@ -12,13 +12,22 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('questions', QuestionController::class);
-Route::apiResource('answers', AnswerController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('questions', QuestionController::class)->except(['index', 'show']);
+    Route::apiResource('answers', AnswerController::class)->except(['index', 'show']);
+    Route::put('answers/{answer}/incrementvote', [AnswerController::class, 'incrementVote']);
+    Route::put('answers/{answer}/decrementvote', [AnswerController::class, 'decrementVote']);
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('tags', TagController::class)->except(['index']);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
+
+
+Route::apiResource('questions', QuestionController::class)->only(['index', 'show']);
+Route::apiResource('answers', AnswerController::class)->only(['index', 'show']);
 //Route::put('answers/{answer}/validate', [AnswerController::class, 'is_validated']);
-Route::put('answers/{answer}/incrementvote', [AnswerController::class, 'incrementVote'])->middleware('auth:sanctum');
-Route::put('answers/{answer}/decrementvote', [AnswerController::class, 'decrementVote'])->middleware('auth:sanctum');
 Route::apiResource('tags', TagController::class);
-Route::apiResource('users', UserController::class);
+
 
 
 Route::post('register', [AuthController::class, 'register']);
